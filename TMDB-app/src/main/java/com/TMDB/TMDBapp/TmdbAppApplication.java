84ccba46cc68ff331aa70f1f5a23335b601d3c2c
John Limkeman <68929092+john-limkeman@users.noreply.github.com>
2021-03-18 11:24:12 -0500

    Add files via upload

---
+++ TMDB-app/src/main/java/com/TMDB/TMDBapp/TmdbAppApplication.java
@@ -0,0 +1,13 @@
+package com.TMDB.TMDBapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbAppApplication.class, args);
+	}
+
+}
---
+++ TMDB-app/src/test/java/com/TMDB/TMDBapp/TmdbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMDB.TMDBapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
